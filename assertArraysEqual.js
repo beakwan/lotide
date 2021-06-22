f1f97e7d255362f0cee72396ddcf34a69f0749fd
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-06-22 20:56:44 +0000

    Function to print array equality assertions

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,16 @@
-const eqArrays = function(array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
     for (let j = 0; j < array2.length; j++) {
       if (array1[i] === array2[j]) {
-        return true;
+        console.log('✅ These arrays are equal!');
+        return;
       } else {
-        return false;
+        console.log('🛑 These arrays are not equal!');
+        return;
       }
     }
   }
 }
 
-const assertArraysEqual = function(arrays) {
-  if (arrays) {
-    console.log('✅ These arrays are equal!');
-  } else {
-    console.log('🛑 These arrays are not equal!');
-  }
-}
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
-assertArraysEqual(eqArrays([2, 3, 5], [6, 5, 4]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([2, 3, 5], [6, 5, 4]);
