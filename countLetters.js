eabd94f4efa5e87d06bdff4f84e3edc30ff380cd
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-06-23 19:54:48 +0000

    add my count letters function

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(string) {
+  let count = {};
+  for (const letter of string) {
+    if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  return count;
+};
+
+
+console.log(countLetters('hello world'))
