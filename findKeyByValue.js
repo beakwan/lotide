90fa2387e40de53b996ced9e4652ec6ef1e9a932
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-06-24 00:45:29 +0000

    initial commit of find key by value function

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  const keys = Object.keys(object);
+  for (const property of keys) {
+   if (object[property] === value) {
+     return property;
+   }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+console.log(findKeyByValue(bestTVShowsByGenre, 'The Wire'));
