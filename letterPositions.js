48e9b8029e29b7d8a797cf3d70394d15ea75380f
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-06-24 19:34:28 +0000

    updated with new assert arrays eq

--- letterPositions.js
+++ letterPositions.js
@@ -1,16 +1,11 @@
-const assertArraysEqual = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    for (let j = 0; j < array2.length; j++) {
-      if (array1[i] === array2[j]) {
-        console.log('✅ These arrays are equal!');
-        return;
-      } else {
-        console.log('🛑 These arrays are not equal!');
-        return;
-      }
-    }
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅ Assertion passed! ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑 Assertion failed. ${actual} !== ${expected}`)
   }
 };
+
 
 const eqArrays = function(array1, array2) {
   for (let i = 0; i < array1.length; i++) {
