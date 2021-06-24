ed80441e313e091f6ba49289f0433b49f20b22c1
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-06-24 13:12:15 +0000

    Update on assert array eq and initial commit of assert objects eq

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,31 @@
-const assertArraysEqual = function(array1, array2) {
-  for (let i = 0; i < array1.length; i++) {
-    for (let j = 0; j < array2.length; j++) {
-      if (array1[i] === array2[j]) {
-        console.log('✅ These arrays are equal!');
-        return;
-      } else {
-        console.log('🛑 These arrays are not equal!');
-        return;
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        if (array1[i] === array2[j]) {
+          return true;
+        } else {
+          return false;
+        }
       }
     }
   }
-}
+};
+
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅ Assertion passed! ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑 Assertion failed. ${actual} !== ${expected}`)
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+assertArraysEqual(['hi', 2, 3], [1, 2, 3]);
+assertArraysEqual(['hi', 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual(['2', 3], ['2', 3, 4]);
+
---
+++ assertObjectsEqual.js
@@ -0,0 +1,51 @@
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      for (let j = 0; j < array2.length; j++) {
+        if (array1[i] === array2[j]) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+};
+
+
+
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  } else {
+    for (let property of keys1) {
+      if (Array.isArray(object1[property]) || Array.isArray(object2[property])) {
+        return eqArrays(object1[property], object2[property]);
+      } else {
+        if (object1[property] !== object2[property]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+  if (eqObjects(actual, expected) === true) {
+    console.log(`✅ Assertion passed! ${inspect(actual)} === ${inspect(expected)}`)
+  } else {
+    console.log(`🛑 Assertion failed. ${inspect(actual)} !== ${inspect(expected)}`)
+  }
+};
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
